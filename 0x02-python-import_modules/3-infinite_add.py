bc35b53bf6543197d5ee645308a41a3830caad22
Adekoya Oluwaseyi Taiwo <107028008+Sheyihloistee@users.noreply.github.com>
2022-09-27 16:00:57 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-if __name__ == '__main__':
-    res = 0
-    cnt = 1
-
-    while cnt < len(argv):
-        res += int(argv[cnt])
-        cnt += 1
-    print(res)
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    for i in range(1, len(sys.argv)):
+        sum = sum + int(sys.argv[i])
+    print(sum)
