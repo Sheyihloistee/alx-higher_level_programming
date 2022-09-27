088e94f7068ae609f52318f61eedfe26eefc7c82
Adekoya Oluwaseyi Taiwo <107028008+Sheyihloistee@users.noreply.github.com>
2022-09-27 16:02:18 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-
-
-def principal():
-    for i in dir(hidden_4):
-        if not (i[0] == '_' and i[1] == '_'):
-            print(i)
-
-
 if __name__ == "__main__":
-    principal()
+    import hidden_4
+    name = dir(hidden_4)
+    for i in range(len(name)):
+        if name[i][0] != "_" and name[i][1] != "_":
+            print(name[i])
